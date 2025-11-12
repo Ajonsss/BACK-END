80ae5d8d50e79c88ac590a5e34688509ed07e3ba
Ajonsss <21-06599@g.batstate-e.edu.ph>
2025-11-12 12:35:21 +0800

    User Registration

--- app.js
+++ app.js
@@ -2,15 +2,31 @@ import express from "express";
 import 'dotenv/config.js';
 import bookRoutes from "./routers/BookRoutes.js";
 import studentRoutes from "./routers/StudentRoutes.js";
+import userRoutes from "./routers/UserRoutes.js";
+import cors from "cors";
 
 
 //initialize app
 const app = express();
 
+//Enable cors to frontend
+let corsOptions = {
+    origin: process.env.ORIGIN
+}
+
 const port = 3000;
 
 //middleware
 app.use(express.json());
+app.use(cors(corsOptions));
+
+//This is
+app.use((req, res, next) => {
+    console.log(req.path, req.method);
+    next();
+})
+
+
 
 try{
     app.listen(process.env.PORT || 3000, () => {
@@ -22,3 +38,4 @@ try{
 
 app.use('/book', bookRoutes);
 app.use('/student', studentRoutes);
+app.use('/user', userRoutes);
